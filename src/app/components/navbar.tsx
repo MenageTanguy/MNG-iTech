4e3145352f203fd9209f792d2a922796e38da59e
Tanguy Ménage <tanguy.menage@gmail.com>
2024-01-16 16:42:58 +0100

    adding new pages

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,9 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import NavItem from "./navItem";
+import { faHome, faHomeAlt, faHomeLg } from "@fortawesome/free-solid-svg-icons";
 
 export default function NavBar() {
   const pages = [
-    { text: "Home", href: "/" },
-    { text: "À propos", href: "/a-propos" },
+    { text: <FontAwesomeIcon icon={faHomeLg} />, href: "/" },
+    { text: "À propos", href: "/about" },
     { text: "Services", href: "/services" },
     { text: "Contact", href: "/contact" },
   ];
